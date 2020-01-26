568a047316f467d477639e6318bb962deaab3d6b
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2020-01-26 18:53:11 +0700

    Update overview.php
    - mengganti base_url
    -merapihkan script js

--- application/views/admin/overview.php
+++ application/views/admin/overview.php
@@ -7,20 +7,14 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Dashboard - SB Admin</title>
-        <link href="<?php echo base_url('css/bootstrap.css') ?>" rel="stylesheet" />
-        <link href="<?php echo base_url('css/bootstrap.min.css') ?>" rel="stylesheet" crossorigin="anonymous" />
-        <link href="<?php echo base_url('css/bootstrap-grid.css') ?>" rel="stylesheet" />
-        <link href="<?php echo base_url('css/bootstrap-grid.min.css') ?>" rel="stylesheet" />
-        <link href="<?php echo base_url('css/bootstrap-reboot.css') ?>" rel="styleshett" />
-        <link href="<?php echo base_url('css/bootstrap-reboot.min.css') ?>" rel="styleshett" />
-        <script src= "<?php echo base_url('assets/demo/chart-area-demo.js') ?>" crossorigin="anonymous"></script>
-        <script src= "<?php echo base_url('assets/demo/chart-bar-demo.js') ?>"></script>
-        <script src= "<?php echo base_url('assets/demo/chart-pie-demo.js') ?>"></script>
-        <script src= "<?php echo base_url('assets/demo/datatables-demo.js') ?>"></script>
-        <script src= "<?php echo base_url('js/bootstrap.bundle.js') ?>"></script>
-        <script src= "<?php echo base_url('js/bootstrap.bundle.min.js') ?>"></script>
-        <script src= "<?php echo base_url('js/bootstrap.js') ?>"></script>
-        <script src= "<?php echo base_url('js/bootstrap.min.js') ?>"></script>
+        <link href="<?php echo base_url(); ?>css/bootstrap.css" rel="stylesheet" />
+        <link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet" />
+        <link href="<?php echo base_url(); ?>css/bootstrap-grid.css" rel="stylesheet" />
+        <link href="<?php echo base_url(); ?>css/bootstrap-grid.min.css" rel="stylesheet" />
+        <link href="<?php echo base_url(); ?>css/bootstrap-reboot.css" rel="styleshett" />
+        <link href="<?php echo base_url(); ?>css/bootstrap-reboot.min.css" rel="styleshett" />
+
+       
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
@@ -667,13 +661,28 @@
             </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
+        
+        <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script> -->
+        <script src="<?php echo base_url(); ?>js/scripts.js"></script>
+        <script src= "<?php echo base_url(); ?>assets/chart/Chart.bundle.js"></script>
+        <script src= "<?php echo base_url(); ?>assets/chart/Chart.bundle.min.js"></script>
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script> -->
+        <!-- <script src="<?php echo base_url(); ?>assets/demo/chart-area-demo.js"></script> -->
+        <!-- <script src="<?php echo base_url(); ?>assets/demo/chart-bar-demo.js"></script> -->
         <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/datatables-demo.js"></script>
+        <script src="<?php echo base_url(); ?>assets/demo/datatables-demo.js"></script>
+
+        <!-- <script src= "<?php echo base_url(); ?>assets/chart/Chart.bundle.js"></script>
+        <script src= "<?php echo base_url(); ?>assets/chart/Chart.bundle.min.js"></script> -->
+        <script src= "<?php echo base_url(); ?>assets/demo/chart-area-demo.js"></script>
+        <script src= "<?php echo base_url(); ?>assets/demo/chart-area-demo.js"></script>
+        <script src= "<?php echo base_url(); ?>assets/demo/chart-bar-demo.js"></script>
+        <script src= "<?php echo base_url(); ?>assets/demo/chart-pie-demo.js"></script>
+        <script src= "<?php echo base_url(); ?>assets/demo/datatables-demo.js"></script>
+        <script src= "<?php echo base_url(); ?>js/bootstrap.bundle.js"></script>
+        <script src= "<?php echo base_url(); ?>js/bootstrap.bundle.min.js"></script>
+        <script src= "<?php echo base_url(); ?>js/bootstrap.js"></script>
+        <script src= "<?php echo base_url(); ?>js/bootstrap.min.js"></script>
     </body>
 </html>
